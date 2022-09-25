e1cb29bad491b1c7275f3c6333527ae1ff2b707f
Nguyen Duc Tien <neittien0110@gmail.com>
2022-09-25 23:55:54 +0700

    Chuyển đổi thành Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import React from 'react';
 import './App.css';
 import Navigation from './Navigation';
 
 function App() {
   return (
-    <Navigation>
-
-    </Navigation>
+    <Navigation />
   );
 }
 
